bc29d41ef42e73c0d5625e9ee8d93a51b6d01dbf
Marcel Meißner <marcel@Marcels-MBP.fritz.box>
2018-07-09 21:07:53 +0200

    Stack

---
+++ Exercises/stack_machine.rb
@@ -0,0 +1,52 @@
+def push(state)
+  state[:stack] << state[:register]
+end
+
+def pop(state)
+  state[:register] = state[:stack].pop
+end
+
+def add(state)
+  state[:register] += state[:stack].pop
+end
+
+def sub(state)
+  state[:register] -= state[:stack].pop
+end
+
+def div(state)
+  state[:register] = state[:register] / state[:stack].pop
+end
+
+def mult(state)
+  state[:register] = state[:register] * state[:stack].pop
+end
+
+def mod(state)
+  state[:register] = state[:register] % state[:stack].pop
+end
+
+def myprint(state)
+  puts state[:register]
+end
+
+MNEMONIC = { 'PUSH': method(:push), 'POP': method(:pop), 'ADD': method(:add), 'SUB': method(:sub),
+             'MULT': method(:mult), 'DIV': method(:div), 'MOD': method(:mod), 'PRINT': method(:myprint) }.freeze
+
+def minilang(program)
+  state = { stack: [], register: 0, commands: program.split }
+  while !state[:commands].empty?
+    command = state[:commands].pop
+    execute_command(command, state)
+  end
+end
+
+def execute_command(command, state)
+  if /\d/.match?(command)
+    state[:register] = command.to_i
+  else
+    MNEMONIC[command].call(state)
+  end
+end
+
+
